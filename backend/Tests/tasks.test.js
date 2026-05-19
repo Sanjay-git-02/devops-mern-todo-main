c95f5bc83742f8aafb13b4b0173bd2ec971d4d9b
Sanjay <sanjusanjay06406@gmail.com>
2026-05-19 16:30:35 +0530

    Initial Commit

---
+++ backend/Tests/tasks.test.js
@@ -0,0 +1,21 @@
+const request = require("supertest");
+const { server, app } = require("../index");
+const mongoose = require("mongoose");
+
+describe("GET api/tasks", () => {
+  it("it should return tasks", async () => {
+    const res = await request(app).get("/api/tasks");
+    expect(res.statusCode).toBe(200);
+  });
+
+  it("it should check the return data is array",async()=>{
+    const res = await request(app).get("/api/tasks");
+    // expect(Array.isArray(res.body)).toBe(true);
+    expect(typeof res.body).toBe("object")
+  })
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  await server.close();
+});
