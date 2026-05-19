b9116c6e8563ae47ccacf1a41014e272fc06940a
Sanjay <sanjusanjay06406@gmail.com>
2026-05-19 19:25:41 +0530

    Fix tasks.test.js

--- backend/Tests/tasks.test.js
+++ backend/Tests/tasks.test.js
@@ -8,11 +8,12 @@ describe("GET api/tasks", () => {
     expect(res.statusCode).toBe(200);
   });
 
-  it("it should check the return data is array",async()=>{
+  it("it should check the return data is array", async () => {
     const res = await request(app).get("/api/tasks");
     // expect(Array.isArray(res.body)).toBe(true);
-    expect(typeof res.body).toBe("object")
-  })
+    expect(typeof res.body).toBe("object");
+    console.log(res.body.tasks);
+  });
 });
 
 afterAll(async () => {
